669a3453f875f82d16b6085a97f9d355009ef5bb
Meysson17 <102948238+Meysson17@users.noreply.github.com>
2023-01-26 09:42:21 +0100

    first

---
+++ src/main/java/fr/eni/film/filmo/FilmoApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.film.filmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/film/filmo/FilmoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.film.filmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
